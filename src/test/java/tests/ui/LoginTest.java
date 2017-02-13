e9d8adca707d72fdcdcbb92799bbe1fe840823f6
syakkala <subbu_yakkala@hotmail.com>
2017-02-12 20:15:34 -0800

    Added UI test case in Login

--- src/test/java/tests/ui/LoginTest.java
+++ src/test/java/tests/ui/LoginTest.java
@@ -1,6 +1,11 @@
 package tests.ui;
 
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -10,20 +15,20 @@ import org.testng.annotations.Test;
 import bl.pages.DashboardPage;
 import bl.pages.LoginPage;
 
-public class LoginTest extends TestBase {
+public class LoginTest {
 	
-	@BeforeTest
-	public void openBrowser()
+	@Test
+	public void mcLogin() throws InterruptedException
 	{
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20L, TimeUnit.SECONDS);
 		driver.get("https://home.mcafee.com/secure/protected/login.aspx");
-	}
-
-	@Test(groups={"ui","mcLogin","login","high"})
-	public void mcLogin()
-	{
-		LoginPage lp = PageFactory.initElements(driver, LoginPage.class);
-		DashboardPage dp = lp.doLogin("tafsample@mcafee.com", "Poiu0987");
-		Assert.assertEquals("My Account",dp.getTitle());
-		dp.doLogOut();
+		driver.findElement(By.name("UserID")).sendKeys("tafsample@mcafee.com");
+		driver.findElement(By.name("Password")).sendKeys("Poiu0987");
+		driver.findElement(By.name("btnLogin")).click();
+		Thread.sleep(1000L);
+		driver.quit();
 	}
 }
